beda13509afbc7aeeb81df6be9be25f49fdb561b
Andrey Bogdanov <bay73@mail.ru>
2020-08-18 14:57:37 +0200

    Add card header for results

--- routes/index.js
+++ routes/index.js
@@ -20,13 +20,21 @@ router.get('/championship/:id', async (req, res, next) => {
     const types = await db.query('SELECT * FROM contest_types');
     const contests = await db.query('SELECT * FROM contests');
     var results = [];
+    var contest = null;
     if (req.params.id) {
-      results = await db.query('SELECT p.id, p.name, c.country, r.official_rank,  r.unofficial_rank, r.points'+
-                                ' FROM results r, participants p, countries c '+
-                               ' WHERE r.contest_id = ? AND p.id = r.participant_id AND c.id = p.countryid', req.params.id);
+      const result = await db.query('SELECT ct.name, c.label FROM contests c, contest_types ct WHERE ct.id = c.contest_typeid and c.id = ?', req.params.id);
+      if (result.length != 0) {
+        contest = transformRawList(result)[0];
+      }
+      if (contest) {
+        results = await db.query('SELECT p.id, p.name, c.country, r.official_rank,  r.unofficial_rank, r.points'+
+                                  ' FROM results r, participants p, countries c '+
+                                 ' WHERE r.contest_id = ? AND p.id = r.participant_id AND c.id = p.countryid', req.params.id);
+      }
     }
     res.render('championship',
       {
+         contest: contest,
       	 types: transformRawList(types),
       	 contests: transformRawList(contests),
       	 results: transformRawList(results),
@@ -41,13 +49,22 @@ router.get('/participant/:id', async (req, res, next) => {
     const countries = await db.query('SELECT * FROM countries');
     const participants = await db.query('SELECT * FROM participants');
     var results = [];
+    var participant = null;
     if (req.params.id) {
-      results = await db.query('SELECT c.id, ct.name, c.label, r.official_rank,  r.unofficial_rank, r.points'+
-                                ' FROM results r, contests c, contest_types ct '+
-                               ' WHERE r.participant_id = ? AND c.id = r.contest_id AND ct.id = c.contest_typeid', req.params.id);
+      const result = await db.query('SELECT name FROM participants WHERE id = ?', req.params.id);
+      if (result.length != 0) {
+        participant = transformRawList(result)[0];
+      }
+      if (participant) {
+        results = await db.query('SELECT c.id, ct.name, c.label, r.official_rank,  r.unofficial_rank, r.points'+
+                                  ' FROM results r, contests c, contest_types ct '+
+                                 ' WHERE r.participant_id = ? AND c.id = r.contest_id AND ct.id = c.contest_typeid', req.params.id);
+      }
     }
+    console.log(participant);
     res.render('participant',
       {
+         participant: participant,
       	 countries: transformRawList(countries),
       	 participants: transformRawList(participants),
       	 results: transformRawList(results),
